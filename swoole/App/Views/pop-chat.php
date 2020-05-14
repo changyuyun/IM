3fd5b1111c9ac9f016416dd8de4c79d9dbfc813a
changyuyun <969842624@qq.com>
2020-05-14 14:28:17 +0800

    点对点聊天界面

--- swoole/App/Views/pop-chat.php
+++ swoole/App/Views/pop-chat.php
@@ -39,7 +39,7 @@
                             <img :src="user.avatar" alt="">
                         </div>
                         <div class="online_status">
-                            <div class="online_username">cyy</div>
+                            <div class="online_username">{{user.username}}</div>
                         </div>
                     </template>
                 </div>
@@ -131,7 +131,7 @@
             HeartBeatTimer   : null,
             ReconnectBox     : null,
             currentUser      : {username: 'ITYUN', intro: 'ityun tech', fd: 0, avatar: "https://www.gravatar.com/avatar/8661f689eba9cb2c6a4305d8046a7e15"},
-            roomUser         : {},
+            roomUser         : [{"avatar":"https://www.gravatar.com/avatar/8661f689eba9cb2c6a4305d8046a7e15","username":"yy"}],
             roomChat         : [],
             up_recv_time     : 0
         }
