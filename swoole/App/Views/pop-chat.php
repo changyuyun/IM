185f4c6d16aeda9039d10970ff661ef1d486bc5b
changyuyun <969842624@qq.com>
2020-05-14 11:34:21 +0800

    点对点聊天界面

---
+++ swoole/App/Views/pop-chat.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>点对点聊天</title>
+    <link rel="stylesheet" href="https://cdn.staticfile.org/amazeui/2.7.2/css/amazeui.min.css">
+    <link rel="stylesheet" href="https://cdn.staticfile.org/layer/2.3/skin/layer.css">
+    <link rel="stylesheet" href="/css/main.css?v=120203">
+    <script src="https://cdn.staticfile.org/vue/2.5.17-beta.0/vue.js"></script>
+    <script src="https://cdn.staticfile.org/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/layer/2.3/layer.js"></script>
+</head>
+<body>
+123
+</body>
+</html>
